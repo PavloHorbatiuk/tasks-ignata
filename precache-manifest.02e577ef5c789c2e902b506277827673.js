cda2c5f77952c578a5188b944db51d5849ba1d25
Паша Горбатюк <92729703+PavloHorbatiuk@users.noreply.github.com>
2022-07-14 20:01:12 +0300

    Updates

---
+++ precache-manifest.02e577ef5c789c2e902b506277827673.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "392eb77971d1d21dbe27e841149ad059",
+    "url": "/tasks-ignata/index.html"
+  },
+  {
+    "revision": "a26e891b59be6810c1b3",
+    "url": "/tasks-ignata/static/css/main.414d99ff.chunk.css"
+  },
+  {
+    "revision": "f612e9bb3393c0a7f583",
+    "url": "/tasks-ignata/static/js/2.26bfee01.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/tasks-ignata/static/js/2.26bfee01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a26e891b59be6810c1b3",
+    "url": "/tasks-ignata/static/js/main.60ba87d8.chunk.js"
+  },
+  {
+    "revision": "46785808a7234a4893f2",
+    "url": "/tasks-ignata/static/js/runtime-main.c370a36b.js"
+  },
+  {
+    "revision": "a6936a0e7f737be86affa38d93192ccb",
+    "url": "/tasks-ignata/static/media/cosmos.a6936a0e.svg"
+  }
+]);
